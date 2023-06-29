14b006475a31f6a15ffa25b0dde4dd78944719fc
aadnekar <aadne.karstad@gmail.com>
2023-06-29 13:15:33 +0200

    Format and linting

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::{env, path::Path, process::{Command, exit, abort}};
 use clap::Parser;
 use install_shared::options::SharedOptions;
+use std::{
+    env,
+    path::Path,
+    process::{abort, exit, Command},
+};
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let opts: SharedOptions = SharedOptions::parse().try_into()?;
+    let opts: SharedOptions = SharedOptions::parse();
     let shared_workspace = env::var("SharedWorkspace").expect("Environment variable SharedWorkSpace not set.\nYou need to set up an environment variable called\"SharedWorkspace\" with path to pc-nrfconnect-shared.");
 
     if opts.generate_types {
         println!("Generating types.");
 
-        let generate_types_output = Command::new("npm").current_dir(shared_workspace.clone()).args(&["run", "generate-types"]).output().expect("failed to execute process");
+        let generate_types_output = Command::new("npm")
+            .current_dir(shared_workspace.clone())
+            .args(["run", "generate-types"])
+            .output()
+            .expect("failed to execute process");
         if generate_types_output.status.success() {
             println!("Types generated.");
         } else {
@@ -20,14 +28,20 @@ fn main() -> Result<()> {
     }
 
     println!("Packing pc-nrfconnect-shared.");
-    let pack_output = Command::new("npm").current_dir(shared_workspace.clone()).arg("pack").output().expect("failed to execute process");
+    let pack_output = Command::new("npm")
+        .current_dir(shared_workspace.clone())
+        .arg("pack")
+        .output()
+        .expect("failed to execute process");
 
     println!("Packed: {}", String::from_utf8_lossy(&pack_output.stdout));
     if !pack_output.status.success() {
         println!("stderr: {}", String::from_utf8_lossy(&pack_output.stderr));
         abort();
     }
-    let tarball = String::from_utf8_lossy(&pack_output.stdout).trim().to_string();
+    let tarball = String::from_utf8_lossy(&pack_output.stdout)
+        .trim()
+        .to_string();
 
     let tarball = Path::join(Path::new(&shared_workspace), Path::new(&tarball));
     if !tarball.exists() {
@@ -35,7 +49,10 @@ fn main() -> Result<()> {
     }
 
     let tarball = tarball.to_str().unwrap();
-    let npm_install = Command::new("npm").args(&["install", tarball]).output().expect("Failed to install {shared_workspace}{tarball}");
+    let npm_install = Command::new("npm")
+        .args(["install", tarball])
+        .output()
+        .expect("Failed to install {shared_workspace}{tarball}");
     if npm_install.status.success() {
         println!("Installed {tarball}");
         exit(0);
@@ -43,5 +60,5 @@ fn main() -> Result<()> {
 
     println!("Failed to install {tarball}");
     println!("stderr: {}", String::from_utf8_lossy(&npm_install.stderr));
-    return Ok(());
+    Ok(())
 }
--- src/options.rs
+++ src/options.rs
@@ -3,7 +3,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap()]
 pub struct SharedOptions {
-    
-    #[clap(short = 't', long = "types", default_value = "false", help = "Generates types using 'npm run generate-types'")]
+    #[clap(
+        short = 't',
+        long = "types",
+        default_value = "false",
+        help = "Generates types using 'npm run generate-types'"
+    )]
     pub generate_types: bool,
 }
